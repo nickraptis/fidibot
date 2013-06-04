ecdb5d4391bcf445e2ae7cc5c4c195f30ec3e891
Nick Raptis <airscorp@gmail.com>
2013-06-04 04:59:50 +0300

    urlparse: Organize code and tests
    Moved the cod around a bit and wrote the regex into verbose format.
    Also started some tests.
    To run them, just run the file by itself.

--- src/modules/urlparser.py
+++ src/modules/urlparser.py
@@ -7,13 +7,24 @@ import re
 import requests
 from basemodule import BaseModule, BaseCommandContext
 
+regex = re.compile("""
+                   ^(                # Starts with
+                    https?://|       # http:// or https:// or
+                    www\.            # www.
+                   )\S+$             # more characters until the end
+                   """, re.IGNORECASE | re.VERBOSE)
+
+def is_url(token):
+    """Return true if the input is a URL"""
+    return regex.match(token)
+
 
 class UrlParserContext(BaseCommandContext):
 
-    def parse_url(self, txt):
+    def parse_urls(self, txt):
         """Return a list of all urls in line of text"""
-        result = re.findall(r'www\S+|https?\S+', txt)
-        return result
+        urls = [w for w in txt.split() if is_url(w)]
+        return urls
 
     def find_url_title(self, url):
         """Retrieve the title of a given URL"""
@@ -34,7 +45,7 @@ class UrlParserContext(BaseCommandContext):
             start = cmphtml.find("<title>")
             end = cmphtml.find("</title>")
             if start == -1 or end == -1:
-                return "Could not find page title!"
+                return resp.url, "Could not find page title!"
             else:
                 html = html[start+7:end]
                 return resp.url, html.strip()
@@ -43,7 +54,7 @@ class UrlParserContext(BaseCommandContext):
         """Try to find URLs in every line and send back their title"""
         if super(UrlParserContext, self).do_public():
             return True
-        urls = self.parse_url(self.input)
+        urls = self.parse_urls(self.input)
         if urls:
             for url in urls:
                 final_url, title = self.find_url_title(url)
@@ -51,8 +62,8 @@ class UrlParserContext(BaseCommandContext):
         return True
 
     def cmd_title(self, argument):
-        """Parse argument for urls and return page title."""
-        urls = self.parse_url(argument)
+        """Treat the argument as urls and return page title."""
+        urls = argument.split()
         target = self.channel if self.channel.startswith("#") else self.nick
         if urls:
             for url in urls:
@@ -67,3 +78,24 @@ class UrlParserModule(BaseModule):
     context_class = UrlParserContext
 
 module = UrlParserModule
+
+
+# testing the regex #
+#####################
+should_be_urls = ('http://google.com', 'www.google.com',)
+should_not_be = ('asdadadasda',)
+
+def test_regex():
+    for i in should_be_urls:
+        if is_url(i):
+            print "PASS: %s worked" % i
+        else:
+            print "FAIL: %s should have worked" % i
+    for i in should_not_be:
+        if not is_url(i):
+            print "PASS: %s failed as expected" % i
+        else:
+            print "FAIL: %s should have failed" % i
+
+if __name__ == '__main__':
+    test_regex()
