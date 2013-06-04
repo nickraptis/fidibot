e0a01352837c4ed5bfb54e960e879b3f639e36aa
Nick Raptis <airscorp@gmail.com>
2013-06-04 05:31:56 +0300

    Added ignore module. Closes #9
    Very simple implementation.
    Should be ebough.
    Added the other bot to the list, as this is the reason we wanted this.

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -12,7 +12,7 @@ Base classes live in the 'basemodule' file.
 """
 
 # define modules to get functionality from
-active = ["basiccmds", "urlparser"]
+active = ["ignore", "basiccmds", "urlparser"]
 
 
 def active_modules():
---
+++ src/modules/ignore.py
@@ -0,0 +1,33 @@
+# Author: Nick Raptis <airscorp@gmail.com>
+"""
+Module to ignore certain users
+
+Just a simple list based ignore.
+Place this module early in the active chain.
+If the user matches, the message will stop right there.
+"""
+
+from basemodule import BaseModule, BaseContext
+
+
+ignore_list = ('fossbot', 'fossbot_')
+
+class IgnoreContext(BaseContext):
+
+    def do_public(self):
+        return self._do()
+
+    def do_private(self):
+        return self._do()
+
+    def _do(self):
+        if self.nick in ignore_list:
+            return True
+        else:
+            return False
+
+
+class IgnoreModule(BaseModule):
+    context_class = IgnoreContext
+
+module = IgnoreModule
