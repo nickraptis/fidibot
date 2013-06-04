48a25298c01c936ec23af8f349ba9e8a2e27f7a6
Nick Raptis <airscorp@gmail.com>
2013-06-04 20:52:52 +0300

    Add template file for modules
    This is it.

---
+++ src/modules/template.py
@@ -0,0 +1,57 @@
+# Author: Your Name here <your@email.com>
+"""
+Template file for making bot modules
+
+You should make a docstring yourself here to decribe your bot.
+The first line should be concise as it will be used by help.
+
+Don't forget to name of your module in modules/__init__.py
+Only the first part of the filename is needed,
+ie. "template" for template.py
+"""
+
+from basemodule import BaseModule, BaseCommandContext
+# if you want an advanced bot, import BaseContext and instead
+# of making commands, override do_public() and do_private()
+
+from alternatives import _
+
+alternatives_dict = {
+# Enter alternatives for your strings here like such
+
+#     'Welcome %s': [
+#         'Welcome %s',
+#         'Hello %s',
+#     ]
+}
+
+
+class TemplateContext(BaseCommandContext):
+    # No docstring needed here
+    # Read modules/basemodule for documentation
+    # and modules/basiccmds.py for an example
+    
+    def cmd_example(self, argument):
+        """
+        An example command. Just name it cmd_<command name>.
+        
+        Add a docstring like this one for help to use it
+        
+        You can instead end your command in _public or _private
+        so it only works for channel or private messages
+        """
+        # This is a nice recipe to get either the channel or the user,
+        # if your command works both in channel and private
+        target = self.channel if self.channel.startswith("#") else self.nick
+        # Send something back
+        # You should always use a formatted string so the bot can log easily
+        # and you can use alternatives by wrapping your format string in _()
+        self.send(target, _("%s"), argument)
+
+
+# Now subclass Base Module and change it's context_class to your own
+class TemplateModule(BaseModule):
+    context_class = TemplateContext
+
+# Set module to your Module subclass. Congrats!
+module = TemplateModule
