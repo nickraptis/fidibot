f268fd43f641c38935e5f75500ad2a762d56a045
Giannis Giannakopoulos <erevos@erevos.lan>
2013-06-05 19:03:28 +0300

    Fail Module

---
+++ src/modules/fail.py
@@ -0,0 +1,28 @@
+# Author: Giannakopoulos John <giannakopoulosj@gmail.com>
+"""Module for fail quotes"""
+
+from basemodule import BaseModule, BaseCommandContext
+
+from alternatives import _
+
+alternatives_dict = {
+     'FAIL mofa': [
+         'Den se grafo oute aristera oute dexia SE GRAFO STO KENTRO POUXO XORO',
+         'tris pente eikosipente kai 10 sarantadio dose kai ena loukoumaki ston kirio',
+     ]
+}
+
+
+class FailContext(BaseCommandContext):
+    
+    def cmd_fail_public(self, argument):
+        """Give a random FAIL quote :-)"""
+        target = self.channel
+        self.send(target, _("FAIL mofa"))
+
+
+class FailModule(BaseModule):
+    context_class = FailContext
+
+
+module = FailModule
