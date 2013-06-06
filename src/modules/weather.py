4d261f443bce4e685fe35091e82c012a7ad76580
Nick Raptis <airscorp@gmail.com>
2013-06-07 01:06:00 +0300

    weather: add error handling and format code
    pywapi is nice enough to handle exceptions itself,
    and putting the error in the returned dict, which
    we now use.
    Also marked some strings as alternatives and
    did a bit of cleanup and code formatting.

--- src/modules/weather.py
+++ src/modules/weather.py
@@ -5,28 +5,34 @@ from alternatives import _
 
 class WeatherContext(BaseCommandContext):
 
-    def cmd_keros_public(self, argument):
+    def cmd_keros(self, argument):
         """Gives The Temperature and Weather of Nafpaktos """
-
-        #Get the weather for Nafpaktos
+        target = self.channel if self.channel.startswith("#") else self.nick
+        
+        # Get the weather for Nafpaktos
         nafpaktos = pywapi.get_weather_from_weather_com('GRXX1283:1')
-
-        #Get the Current Weather
-        curcond = nafpaktos.get('current_conditions')
+        
+        # Check for errors
+        error = nafpaktos.get('error')
+        if error:
+            self.logger.error(error)
+            self.send(target, _("Error retrieving data: %s"), error)
+            return
+        
+        # Get the Current Weather
+        curcond = nafpaktos.get('current_conditions', {})
         temp = curcond.get('temperature')
         text = curcond.get('text')
         time = curcond.get('last_updated')
         
         fmt_str = _("The Temperature is %s\nThe Weather is %s\nNafpaktos %s")
-        target = self.channel
-        self.send(target, fmt_str, temp, text, time)
+        self.send(target, fmt_str, temp, _(text), time)
 
     def cmd_kairos(self, argument):
         """Gives The Temperature and Weather of Nafpaktos """
-        self.keros(argument)
+        self.cmd_keros(argument)
 
 class WeatherModule(BaseModule):
         context_class = WeatherContext
 
 module = WeatherModule
-
